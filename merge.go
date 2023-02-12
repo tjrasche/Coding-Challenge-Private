44a968be8ae55549394f7780d6f4adda6625943a
tjark <t@rasche-thalhofer.cloud>
2023-02-12 21:47:25 +0100

    added performance test

--- merge.go
+++ merge.go
@@ -2,7 +2,7 @@ package main
 
 import "sort"
 
-// Merge merges all [Interval] together, wehen they have overlapping values, returns values sorted by LowerBound of the Intervals
+// Merge merges all [Interval] together when they have overlapping values, returns values sorted by LowerBound of the Intervals
 func Merge(intervals ByLowerBound) []*Interval {
 	// first we have to sort all intervals. this is done using the [sort.Sort] method provided by go.
 	// sort.Sort uses the methods defined for [ByLowerBound] and described by the [sort.Intercace] to sort the values in an ascending matter
@@ -11,23 +11,23 @@ func Merge(intervals ByLowerBound) []*Interval {
 	var currentInterval = Interval{}
 	var mergedIntervals []*Interval
 
-	for i, interval := range intervals {
+	for i, unmergedInterval := range intervals {
 
 		if i == 0 {
 			// we are at the start of the for loop so we have to set the first upper and lower bound and can move on
-			currentInterval = *NewInterval(interval.LowerBound, interval.UpperBound)
+			currentInterval = *NewInterval(unmergedInterval.LowerBound, unmergedInterval.UpperBound)
 			continue
 		}
-		if interval.LowerBound < currentInterval.UpperBound {
+		if unmergedInterval.LowerBound < currentInterval.UpperBound {
 			// the following interval is overlapping with the previous one, so we have to enlargen our currentInterval, if the upperBound of the interval is larger than the currentinterval
-			if interval.UpperBound > currentInterval.UpperBound {
-				currentInterval.UpperBound = interval.UpperBound
+			if unmergedInterval.UpperBound > currentInterval.UpperBound {
+				currentInterval.UpperBound = unmergedInterval.UpperBound
 			}
 		} else {
 			// we don't have any overlap more. so the currentInterval can be considered "done" and appendend to the resultSLice
 			mergedIntervals = append(mergedIntervals, NewInterval(currentInterval.LowerBound, currentInterval.UpperBound))
 			// as we are a starting a new currentInterval, we have to set it's
-			currentInterval = *NewInterval(interval.LowerBound, interval.UpperBound)
+			currentInterval = *NewInterval(unmergedInterval.LowerBound, unmergedInterval.UpperBound)
 		}
 	}
 	// we still have to add the last interval
--- merge_test.go
+++ merge_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"math/rand"
 	"testing"
+	"time"
 )
 
 // This is a basic first test using the values provided in teh coding challenge to test the merge algorithm
@@ -25,4 +27,44 @@ func TestMergeExample(t *testing.T) {
 			t.Errorf("Expected Values to be equivalent and in order! Expected value: %s, value reveived: %s", *interval, *mergedIntervals[i])
 		}
 	}
+}
+
+// Test to measure performance of the sorting. Consecetuvely merges larger and larger values and measures the time they take.
+// this is done, so a tester can roughly evaluate the growth rate of the execution time of the merge algorithm.
+func TestPerformance(t *testing.T) {
+	// slowly let the result size grow
+	interValCounts := []int{10, 100, 1000, 10_000, 100_000, 1_000_000}
+	var results []time.Duration
+	for _, interValCount := range interValCounts {
+		results = append(results, timedMergeWithRandomIntervals(t, interValCount))
+	}
+	var execTimeQuotients []float64
+	for i, duration := range results {
+		execTimeQuotients = append(execTimeQuotients, float64(duration.Nanoseconds())/float64(interValCounts[i]))
+
+		// we expect a runtime behaviour of O(n*log(n)), so we can fail if the time per interval does not drop with bigger intervals
+		if i != 0 && (execTimeQuotients[i-1] < execTimeQuotients[i]) {
+			t.Errorf("Runtime scaling worse than linear")
+		}
+	}
+}
+
+// generates n random intervals and merges them. Returns the execution time of the merge itself.
+func timedMergeWithRandomIntervals(t *testing.T, n int) (execTime time.Duration) {
+	// generate a very large slice of intervals
+	rand.Seed(time.Now().UnixNano())
+
+	const intervalsCount = 10_000_000
+	var intervals []*Interval
+
+	for i := 0; i < intervalsCount; i++ {
+		lowerBound := rand.Intn(100000)
+		upperBound := lowerBound + rand.Intn(100000)
+		intervals = append(intervals, NewInterval(lowerBound, upperBound))
+	}
+
+	// only measure the time the actual merging takes. Not the random generation of intervals
+	startTime := time.Now()
+	Merge(intervals)
+	return time.Now().Sub(startTime)
 }
