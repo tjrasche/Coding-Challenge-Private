29eb805ecee7090c93b44ec2413f087e9247ede2
tjark <t@rasche-thalhofer.cloud>
2023-02-13 23:28:22 +0100

    added test for negative number merging

--- merge_test.go
+++ merge_test.go
@@ -10,10 +10,26 @@ import (
 func TestMergeExample(t *testing.T) {
 
 	intervals := []*Interval{NewInterval(25, 30), NewInterval(2, 19), NewInterval(14, 23), NewInterval(4, 8)}
+	mergedIntervalsExpectedResult := []*Interval{NewInterval(2, 23), NewInterval(25, 30)}
 
 	mergedIntervals := Merge(intervals)
 
-	mergedIntervalsExpectedResult := []*Interval{NewInterval(2, 23), NewInterval(25, 30)}
+	// check if results from Merge() are equivalent to expected case
+	if len(mergedIntervals) != len(mergedIntervalsExpectedResult) {
+		t.Errorf("Expected lengths of result to be equal")
+	}
+
+	for i, interval := range mergedIntervalsExpectedResult {
+		if !(*interval == *mergedIntervals[i]) {
+			t.Errorf("Expected Values to be equivalent and in order! Expected value: %s, value reveived: %s", *interval, *mergedIntervals[i])
+		}
+	}
+}
+
+func TestMergeNegativeNumbers(t *testing.T) {
+	intervals := []*Interval{NewInterval(-100, -20), NewInterval(-50, -5), NewInterval(0, 20)}
+	mergedIntervalsExpectedResult := []*Interval{NewInterval(-100, -5), NewInterval(0, 20)}
+	mergedIntervals := Merge(intervals)
 
 	// check if results from Merge() are equivalent to expected case
 	if len(mergedIntervals) != len(mergedIntervalsExpectedResult) {
